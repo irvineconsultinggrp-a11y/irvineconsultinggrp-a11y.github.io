75a0e3e5a2449fe42caa8216e83d7a4946b3d1b6
Khang Nguyen <khangnguyen@Khangs-MacBook-Air-3.local>
2026-09-21 04:54:44 -0700

    Increase ICG logo size in footer

--- icg/src/components/Footer.js
+++ icg/src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
             alt="ICG Logo"
             loading="lazy"
             decoding="async"
-            className="w-16 h-16"
+            className="w-24 h-24"
             whileHover={{ rotate: -6, scale: 1.06 }}
             transition={{ type: "spring", stiffness: 300, damping: 18 }}
           />
